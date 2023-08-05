0b2c7c2d4c5bf092094d7866772aabd9c6bd53e0
l34nlin <picassoplay8@gmail.com>
2023-08-04 21:17:47 -0300

    egreg

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MyButton from './src/components/MyButton';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const style = StyleSheet.create({
+  container:{
+      paddingTop:20,
+  },
+  text:{
+      fontSize:18,
+      color: #f4f,
+      fontWeight:'bold'
+  }
+})
 
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <MyButton title={'visualizar'} onPressButton=
+//       {Button}/>
+//       <StatusBar style="auto" />
+
+//     </View>
+//   );
+// }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
